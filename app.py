0f785e54d02844e8489daae1a58157c14e342ba8
ramesh960386 <ramesh.daywell@gmail.com>
2022-01-18 10:17:12 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'This is landing page'
+
+
+@app.route('/page/<id>')
+def page(id):
+    return 'This is page: '+id
+
+
+if __name__ == '__main__':
+    app.run()
